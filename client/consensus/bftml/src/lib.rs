bf2cfa6c7a59240458cf19c0d267b9026f4bf109
Daogang Tang <daogangtang@gmail.com>
2020-03-03 16:37:03 +0800

    wip.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- client/consensus/bftml/src/lib.rs
+++ client/consensus/bftml/src/lib.rs
@@ -93,13 +93,69 @@ mod _app {
 pub type AuthorityPair = _app::Pair;
 pub type AuthoritySignature = _app::Signature;
 pub type AuthorityId = _app::Public;
-pub const RHD_ENGINE_ID: ConsensusEngineId = *b"RHD";
+pub const BFTML_ENGINE_ID: ConsensusEngineId = *b"BFTML";
 
 
-
-pub enum Error {
-
+struct BftmlPreDigest {
+    authority_index: u32
 }
+
+
+#[derive(derive_more::Display, Debug)]
+enum Error<B: BlockT> {
+	#[display(fmt = "Multiple BABE pre-runtime digests, rejecting!")]
+	MultiplePreRuntimeDigests,
+	#[display(fmt = "No BABE pre-runtime digest found")]
+	NoPreRuntimeDigest,
+	#[display(fmt = "Multiple BABE epoch change digests, rejecting!")]
+	MultipleEpochChangeDigests,
+	#[display(fmt = "Could not extract timestamp and slot: {:?}", _0)]
+	Extraction(sp_consensus::Error),
+	#[display(fmt = "Could not fetch epoch at {:?}", _0)]
+	FetchEpoch(B::Hash),
+	#[display(fmt = "Header {:?} rejected: too far in the future", _0)]
+	TooFarInFuture(B::Hash),
+	#[display(fmt = "Parent ({}) of {} unavailable. Cannot import", _0, _1)]
+	ParentUnavailable(B::Hash, B::Hash),
+	#[display(fmt = "Slot number must increase: parent slot: {}, this slot: {}", _0, _1)]
+	SlotNumberMustIncrease(u64, u64),
+	#[display(fmt = "Header {:?} has a bad seal", _0)]
+	HeaderBadSeal(B::Hash),
+	#[display(fmt = "Header {:?} is unsealed", _0)]
+	HeaderUnsealed(B::Hash),
+	#[display(fmt = "Slot author not found")]
+	SlotAuthorNotFound,
+	#[display(fmt = "Secondary slot assignments are disabled for the current epoch.")]
+	SecondarySlotAssignmentsDisabled,
+	#[display(fmt = "Bad signature on {:?}", _0)]
+	BadSignature(B::Hash),
+	#[display(fmt = "Invalid author: Expected secondary author: {:?}, got: {:?}.", _0, _1)]
+	InvalidAuthor(AuthorityId, AuthorityId),
+	#[display(fmt = "No secondary author expected.")]
+	NoSecondaryAuthorExpected,
+	#[display(fmt = "VRF verification of block by author {:?} failed: threshold {} exceeded", _0, _1)]
+	VRFVerificationOfBlockFailed(AuthorityId, u128),
+	#[display(fmt = "VRF verification failed: {:?}", _0)]
+	VRFVerificationFailed(SignatureError),
+	#[display(fmt = "Could not fetch parent header: {:?}", _0)]
+	FetchParentHeader(sp_blockchain::Error),
+	#[display(fmt = "Expected epoch change to happen at {:?}, s{}", _0, _1)]
+	ExpectedEpochChange(B::Hash, u64),
+	#[display(fmt = "Could not look up epoch: {:?}", _0)]
+	CouldNotLookUpEpoch(Box<fork_tree::Error<sp_blockchain::Error>>),
+	#[display(fmt = "Block {} is not valid under any epoch.", _0)]
+	BlockNotValid(B::Hash),
+	#[display(fmt = "Unexpected epoch change")]
+	UnexpectedEpochChange,
+	#[display(fmt = "Parent block of {} has no associated weight", _0)]
+	ParentBlockNoAssociatedWeight(B::Hash),
+	#[display(fmt = "Checking inherents failed: {}", _0)]
+	CheckInherents(String),
+	Client(sp_blockchain::Error),
+	Runtime(sp_inherents::Error),
+	ForkTree(Box<fork_tree::Error<sp_blockchain::Error>>),
+}
+
 
 
 // CML: Consensus Middle Layer
@@ -282,7 +338,7 @@ impl<B, E, Block, RA> Verifier<Block> for BftmlVerifier<B, E, Block, RA> where
 	&mut self,
 	origin: BlockOrigin,
 	header: Block::Header,
-	justification: Option<Justification>,
+	justification: Option<Juxostification>,
 	mut body: Option<Vec<Block::Extrinsic>>,
     ) -> Result<(BlockImportParams<Block>, Option<Vec<(CacheKeyId, Vec<u8>)>>), String> {
 
@@ -499,28 +555,68 @@ fn check_header<B: BlockT + Sized>(
     Ok(CheckedHeader::Checked(header, info))
 }
 
-fn find_pre_digest<B: BlockT>(header: &B::Header) -> Result<BabePreDigest, Error<B>>
+fn find_pre_digest<B: BlockT>(header: &B::Header) -> Result<BftmlPreDigest, Error<B>>
 {
     // genesis block doesn't contain a pre digest so let's generate a
     // dummy one to not break any invariants in the rest of the code
     if header.number().is_zero() {
-	return Ok(BabePreDigest::Secondary {
-	    slot_number: 0,
+	return Ok(BftmlPreDigest {
 	    authority_index: 0,
 	});
     }
 
     let mut pre_digest: Option<_> = None;
     for log in header.digest().logs() {
-	trace!(target: "babe", "Checking log {:?}, looking for pre runtime digest", log);
-	match (log.as_babe_pre_digest(), pre_digest.is_some()) {
-	    (Some(_), true) => return Err(babe_err(Error::MultiplePreRuntimeDigests)),
-	    (None, _) => trace!(target: "babe", "Ignoring digest not meant for us"),
+	trace!(target: "bftml", "Checking log {:?}, looking for pre runtime digest", log);
+	match (log.as_bftml_pre_digest(), pre_digest.is_some()) {
+	    (Some(_), true) => return Err(Error::MultiplePreRuntimeDigests),
+	    (None, _) => trace!(target: "bftml", "Ignoring digest not meant for us"),
 	    (s, false) => pre_digest = s,
 	}
     }
-    pre_digest.ok_or_else(|| babe_err(Error::NoPreRuntimeDigest))
+    pre_digest.ok_or_else(|| Error::NoPreRuntimeDigest)
 }
+
+/// A digest item which is usable with Bftml consensus.
+#[cfg(feature = "std")]
+pub trait CompatibleDigestItem: Sized {
+	fn bftml_pre_digest(seal: BftmlPreDigest) -> Self;
+	fn as_bftml_pre_digest(&self) -> Option<BftmlPreDigest>;
+	// fn bftml_seal(signature: AuthoritySignature) -> Self;
+	// fn as_bftml_seal(&self) -> Option<AuthoritySignature>;
+	// fn as_next_epoch_descriptor(&self) -> Option<NextEpochDescriptor>;
+}
+
+#[cfg(feature = "std")]
+impl<Hash> CompatibleDigestItem for DigestItem<Hash> where
+    Hash: Debug + Send + Sync + Eq + Clone + Codec + 'static
+{
+    fn bftml_pre_digest(digest: BftmlPreDigest) -> Self {
+	DigestItem::PreRuntime(BFTML_ENGINE_ID, digest.encode())
+    }
+
+    fn as_bftml_pre_digest(&self) -> Option<BftmlPreDigest> {
+	self.try_to(OpaqueDigestItemId::PreRuntime(&BFTML_ENGINE_ID))
+    }
+
+    // fn babe_seal(signature: AuthoritySignature) -> Self {
+    //	DigestItem::Seal(BABE_ENGINE_ID, signature.encode())
+    // }
+
+    // fn as_babe_seal(&self) -> Option<AuthoritySignature> {
+    //	self.try_to(OpaqueDigestItemId::Seal(&BABE_ENGINE_ID))
+    // }
+
+    // fn as_next_epoch_descriptor(&self) -> Option<NextEpochDescriptor> {
+    //	self.try_to(OpaqueDigestItemId::Consensus(&BABE_ENGINE_ID))
+    //	    .and_then(|x: super::ConsensusLog| match x {
+    //		super::ConsensusLog::NextEpochData(n) => Some(n),
+    //		_ => None,
+    //	    })
+    // }
+}
+
+
 
 
 //
@@ -580,7 +676,7 @@ pub mod gen {
     pub fn gen_gossip_engine() {
 	// executor is a future runtime executor
 	let executor = ..;
-	let gossip_engine = GossipEngine::new(network.clone(), executor, RHD_ENGINE_ID, validator.clone());
+	let gossip_engine = GossipEngine::new(network.clone(), executor, BFTML_ENGINE_ID, validator.clone());
 	gossip_engine
     }
 
