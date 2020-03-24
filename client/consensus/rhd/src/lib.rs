10f43378c804c57aa9f8e0a172470c9f3191de3e
Daogang Tang <daogangtang@gmail.com>
2020-03-24 19:10:01 +0800

    refactor restructure.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- client/consensus/rhd/src/lib.rs
+++ client/consensus/rhd/src/lib.rs
@@ -29,19 +29,16 @@ where
     B: BlockT + Clone + Eq,
     B::Hash: ::std::hash::Hash,
 {
-    // Agreement<context_instance, te_rx, fe_tx>
-    // te_rx: to engine rx, used in engine
-    // fe_tx: from engine tx, used in engine
-    //agreement: Option<rhododendron::Agreement<RhdContext<B>, UnboundedReceiver<>, UnboundedSender<>>>,
     te_tx: Option<UnboundedSender>, // to engine tx, used in this caller layer
     fe_rx: Option<UnboundedReceiver>, // from engine rx, used in this caller layer
     cm_rx: Option<UnboundedReceiver>,
-    bn_tx: Option<UnboundedReceiver>,
 
     tc_rx: UnboundedReceiver,
     ts_tx: UnboundedSender,
     mb_tx: UnboundedSender,
     ib_rx: UnboundedReceiver,
+    ap_tx: UnboundedSender,
+    gp_rx: UnboundedReceiver,
 
     current_round_block: Option<BlockImportParams>,
     bft_task_running: bool,
@@ -57,76 +54,76 @@ where
     type Error = ();
 
     fn poll(&mut self) -> Poll<(), ()> {
-        if !self.timer_running {
-            self.start_timer();
-        }
+	if !self.timer_running {
+	    self.start_timer();
+	}
 
-        // receive protocol msg from scml, forward it to rhd engine
-        match self.tc_rx.poll()? {
-            Async::Ready(Some(msg)) => {
-                // msg reform
+	// receive protocol msg from scml, forward it to rhd engine
+	match self.tc_rx.poll()? {
+	    Async::Ready(Some(msg)) => {
+		// msg reform
 
-                self.te_tx.unbounded_send(msg);
-            }
-            _ => {}
-        }
+		self.te_tx.unbounded_send(msg);
+	    }
+	    _ => {}
+	}
 
-        // impoted block
-        match self.ib_rx.poll()? {
-            Async::Ready(Some(msg)) => {
-                // stuff to do
-                // something after imported block, make a future to Self::CreateProposal and Self::EvaluateProposal
-                // make a new agreement
+	// impoted block
+	match self.ib_rx.poll()? {
+	    Async::Ready(Some(msg)) => {
+		// stuff to do
+		// something after imported block, make a future to Self::CreateProposal and Self::EvaluateProposal
+		// make a new agreement
 
-                //let block = ...;  // extract from msg
-                // start one bft instance, if none exist
-                if !self.bft_task_running {
-                    self.start_once_bft();
-                }
-                // send this new block to bft task
-                if self.bn_tx.is_some() {
-                    self.bn_tx.unbounded_send(block);
-                }
-            }
-            _ => {}
-        }
+		//let block = ...;  // extract from msg
+		// start one bft instance, if none exist
+		if !self.bft_task_running {
+		    self.start_once_bft();
+		}
+		// send this new block to bft task
+		if self.bn_tx.is_some() {
+		    self.bn_tx.unbounded_send(block);
+		}
+	    }
+	    _ => {}
+	}
 
-        // receive rhd engine protocol msg, forward it to scml
-        if self.fe_rx.is_some() {
-            match self.fe_rx.unwrap().poll()? {
-                Async::Ready(Some(msg)) => {
-                    // msg reform
+	// receive rhd engine protocol msg, forward it to scml
+	if self.fe_rx.is_some() {
+	    match self.fe_rx.unwrap().poll()? {
+		Async::Ready(Some(msg)) => {
+		    // msg reform
 
-                    self.ts_tx.unbounded_send(msg);
-                }
-                _ => {}
-            }
-        }
+		    self.ts_tx.unbounded_send(msg);
+		}
+		_ => {}
+	    }
+	}
 
-        // receive rhd engine protocol msg, forward it to scml
-        if self.cm_rx.is_some() {
-            match self.cm_rx.unwrap().poll()? {
-                Async::Ready(Some(commit_msg)) => {
-                    // stuff to do
-                    self.mb_tx.unbounded_send(msg);
-                }
-                _ => println!(""),
-            }
-        }
+	// receive rhd engine protocol msg, forward it to scml
+	if self.cm_rx.is_some() {
+	    match self.cm_rx.unwrap().poll()? {
+		Async::Ready(Some(commit_msg)) => {
+		    // stuff to do
+		    self.cb_tx.unbounded_send(msg);
+		}
+		_ => println!(""),
+	    }
+	}
 
-        // poll agreement and send to mb_tx channel
-        // XXX: check agreement poll ability
-        // match self.agreement.poll()? {
-        //     Async::Ready(Some(msg)) => {
-        //	// stuff to do
-        //	// the result of poll of agreement is Committed<>, deal with it
-        //	self.mb_tx.unbounded_send(msg);
+	// poll agreement and send to mb_tx channel
+	// XXX: check agreement poll ability
+	// match self.agreement.poll()? {
+	//     Async::Ready(Some(msg)) => {
+	//	// stuff to do
+	//	// the result of poll of agreement is Committed<>, deal with it
+	//	self.mb_tx.unbounded_send(msg);
 
-        //     },
-        //     _ => {}
-        // }
+	//     },
+	//     _ => {}
+	// }
 
-        Ok(Async::NotReady)
+	Ok(Async::NotReady)
     }
 }
 
@@ -136,120 +133,115 @@ where
     B::Hash: ::std::hash::Hash,
 {
     pub fn new(
-        authorities: Vec<AuthorityID>, // needed?
-        tc_rx: UnboundedReceiver,
-        ts_tx: UnboundedSender,
-        mb_tx: UnboundedSender,
-        ib_rx: UnboundedReceiver,
+	authorities: Vec<AuthorityID>, // needed?
+	tc_rx: UnboundedReceiver,
+	ts_tx: UnboundedSender,
+	cb_tx: UnboundedSender,
+	ib_rx: UnboundedReceiver,
+	ap_tx: UnboundedSender,
+	gp_rx: UnboundedReceiver,
     ) -> RhdWorker {
-        // let rhd_context = RhdContext {
-        //     // parent_hash: hash.clone(),
-        //     // cache: self.round_cache.clone(),
-        //     // round_timeout_multiplier: self.round_timeout_multiplier,
-        //     // key: self.key.clone(),
-        //     authorities: authorities,
-        // };
+	// let rhd_context = RhdContext {
+	//     // parent_hash: hash.clone(),
+	//     // cache: self.round_cache.clone(),
+	//     // round_timeout_multiplier: self.round_timeout_multiplier,
+	//     // key: self.key.clone(),
+	//     authorities: authorities,
+	// };
 
-        // let (te_tx, te_rx) = mpsc::unbounded();
-        // let (fe_tx, fe_rx) = mpsc::unbounded();
+	// let (te_tx, te_rx) = mpsc::unbounded();
+	// let (fe_tx, fe_rx) = mpsc::unbounded();
 
-        // let mut agreement = rhododendron::agree(
-        //     rhd_context,
-        //     n,
-        //     max_faulty,
-        //     te_rx,  // input
-        //     fe_tx,  // output
-        // );
+	// let mut agreement = rhododendron::agree(
+	//     rhd_context,
+	//     n,
+	//     max_faulty,
+	//     te_rx,  // input
+	//     fe_tx,  // output
+	// );
 
-        RhdWorker {
-            te_tx: None,
-            fe_rx: None,
-            cm_rx: None,
-            bn_tx: None,
-            tc_rx,
-            ts_tx,
-            mb_tx,
-            ib_rx,
-            current_round_block: None,
-            bft_task_running: false,
-            timer_running: false,
-        }
+	RhdWorker {
+	    te_tx: None,
+	    fe_rx: None,
+	    cm_rx: None,
+
+	    tc_rx,
+	    ts_tx,
+	    cb_tx,
+	    ib_rx,
+	    ap_tx,
+	    gp_rx,
+
+	    current_round_block: None,
+	    bft_task_running: false,
+	    timer_running: false,
+	}
     }
 
     fn start_once_bft(&mut self) {
-        let arc_rhd_worker = Arc::new(self.clone());
+	let arc_rhd_worker = Arc::new(self.clone());
 
-        let rhd_context = RhdContext {
-            authorities: authorities,
-            rhd_worker: arc_rhd_worker,
-        };
+	let rhd_context = RhdContext {
+	    authorities: authorities,
+	    rhd_worker: arc_rhd_worker,
+	};
 
-        let (te_tx, te_rx) = mpsc::unbounded();
-        let (fe_tx, fe_rx) = mpsc::unbounded();
-        let (cm_tx, cm_rx) = mpsc::unbounded();
-        let (bn_tx, bn_rx) = mpsc::unbounded(); // block notify
+	let (te_tx, te_rx) = mpsc::unbounded();
+	let (fe_tx, fe_rx) = mpsc::unbounded();
+	let (cm_tx, cm_rx) = mpsc::unbounded();
 
-        let mut agreement = rhododendron::agree(
-            rhd_context,
-            n,
-            max_faulty,
-            te_rx, // input
-            fe_tx, // output
-        );
 
-        self.te_tx = Some(te_tx);
-        self.fe_rx = Some(fe_rx);
-        self.cm_rx = Some(cm_rx);
-        self.bn_tx = Some(bn_tx);
-        //self.agreement = Some(agreement);
+	let mut agreement = rhododendron::agree(
+	    rhd_context,
+	    n,
+	    max_faulty,
+	    te_rx, // input
+	    fe_tx, // output
+	);
 
-        tokio::spawn(futures::future::poll_fn(move || {
-            arc_rhd_worker.bft_task_running = true;
+	self.te_tx = Some(te_tx);
+	self.fe_rx = Some(fe_rx);
+	self.cm_rx = Some(cm_rx);
 
-            match agreement.poll()? {
-                Async::Ready(Some(commit_msg)) => {
-                    // stuff to do
-                    // the result of poll of agreement is Committed<>, deal with it
-                    cm_tx.unbounded_send(commit_msg);
 
-                    // set back
-                    arc_rhd_worker.bft_task_running = false;
-                    arc_rhd_worker.te_tx = None;
-                    arc_rhd_worker.fe_rx = None;
-                    arc_rhd_worker.cm_rx = None;
-                    arc_rhd_worker.bn_tx = None;
 
-                    return Ok(Async::Ready);
-                }
-                _ => Ok(Async::NotReady),
-            }
+	tokio::spawn(futures::future::poll_fn(move || {
+	    arc_rhd_worker.bft_task_running = true;
 
-            match bn_rx.poll()? {
-                Async::Ready(Some(block)) => {
-                    arc_rhd_worker.current_round_block = Some(block);
-                    //Ok(Async::Ready)
-                }
-                _ => Ok(Async::NotReady),
-            }
+	    match agreement.poll()? {
+		Async::Ready(Some(commit_msg)) => {
+		    // the result of poll of agreement is Committed<>, deal with it
+		    cm_tx.unbounded_send(commit_msg);
 
-            Ok(Async::NotReady)
-        }));
+		    // set back
+		    arc_rhd_worker.bft_task_running = false;
+		    arc_rhd_worker.te_tx = None;
+		    arc_rhd_worker.fe_rx = None;
+		    arc_rhd_worker.cm_rx = None;
+
+		    return Ok(Async::Ready);
+		}
+		_ => Ok(Async::NotReady),
+	    }
+
+	    Ok(Async::NotReady)
+	}));
     }
 
     fn start_timer(&mut self) {
-        let task = Interval::new(Instant::now(), Duration::from_millis(6000))
-            .for_each(|instant| {
-                // when no bft task exist, run one
-                if !self.bft_task_running {
-                    self.start_once_bft();
-                }
-                println!("fire; instant={:?}", instant);
-                Ok(())
-            })
-            .map_err(|e| panic!("interval errored; err={:?}", e));
+	let task = Interval::new(Instant::now(), Duration::from_millis(6000))
+	    .for_each(|instant| {
+		// when no bft task exist, run one
+		if !self.bft_task_running {
+		    self.start_once_bft();
+		}
+		println!("fire; instant={:?}", instant);
+		Ok(())
+	    })
+	    .map_err(|e| panic!("interval errored; err={:?}", e));
 
-        tokio::spawn(task);
-        self.timer_running = true;
+	tokio::spawn(task);
+	self.timer_running = true;
     }
 }
 
@@ -278,78 +270,79 @@ where
     type EvaluateProposal = <P::Evaluate as IntoFuture>::Future;
 
     fn local_id(&self) -> AuthorityId {
-        self.key.public().into()
+	self.key.public().into()
     }
 
     fn proposal(&self) -> Self::CreateProposal {
-        poll_fn(move || {
-            match self.rhd_worker.current_round_block {
-                Some(b) => {
-                    // take it and leave none, must
-                    let b = self.rhd_worker.current_round_block.take();
-                    Async::Ready(b)
-                }
-                None => Async::NotReady,
-            }
-        })
+	//let ask_proposal_msg = ...;
+	self.rhd_worker.ap_tx.unbounded_send( ask_proposal_msg );
+
+	poll_fn(move || {
+	    match self.rhd_worker.gp_rx.poll()? {
+		Async::Ready(Some(proposal)) => {
+		    Async::Ready(proposal)
+		}
+		_ => {}
+	    }
+	})
     }
 
     fn candidate_digest(&self, proposal: &B) -> B::Hash {
-        proposal.hash()
+	proposal.hash()
     }
 
     fn sign_local(&self, message: RhdMessage<B, B::Hash>) -> LocalizedMessage<B> {
-        sign_message(message, &*self.key, self.parent_hash.clone())
+	sign_message(message, &*self.key, self.parent_hash.clone())
     }
 
     fn round_proposer(&self, round: u32) -> AuthorityId {
-        //self.proposer.round_proposer(round, &self.authorities[..])
+	//self.proposer.round_proposer(round, &self.authorities[..])
     }
 
     fn proposal_valid(&self, proposal: &B) -> Self::EvaluateProposal {
-        //self.proposer.evaluate(proposal).into_future()
-        poll_fn(move || Async::Ready(true))
+	//self.proposer.evaluate(proposal).into_future()
+	poll_fn(move || Async::Ready(true))
     }
 
     fn begin_round_timeout(&self, round: u32) -> Self::RoundTimeout {
-        let timeout = self.round_timeout_duration(round);
-        let fut = Delay::new(Instant::now() + timeout)
-            .map_err(|e| Error::from(CommonErrorKind::FaultyTimer(e)))
-            .map_err(Into::into);
+	let timeout = self.round_timeout_duration(round);
+	let fut = Delay::new(Instant::now() + timeout)
+	    .map_err(|e| Error::from(CommonErrorKind::FaultyTimer(e)))
+	    .map_err(Into::into);
 
-        Box::new(fut)
+	Box::new(fut)
     }
 
     fn on_advance_round(
-        &self,
-        accumulator: &rhododendron::Accumulator<B, B::Hash, Self::AuthorityId, Self::Signature>,
-        round: u32,
-        next_round: u32,
-        reason: AdvanceRoundReason,
+	&self,
+	accumulator: &rhododendron::Accumulator<B, B::Hash, Self::AuthorityId, Self::Signature>,
+	round: u32,
+	next_round: u32,
+	reason: AdvanceRoundReason,
     ) {
-        use std::collections::HashSet;
+	use std::collections::HashSet;
 
-        let collect_pubkeys = |participants: HashSet<&Self::AuthorityId>| {
-            participants
-                .into_iter()
-                .map(|p| ::ed25519::Public::from_raw(p.0))
-                .collect::<Vec<_>>()
-        };
+	let collect_pubkeys = |participants: HashSet<&Self::AuthorityId>| {
+	    participants
+		.into_iter()
+		.map(|p| ::ed25519::Public::from_raw(p.0))
+		.collect::<Vec<_>>()
+	};
 
-        let round_timeout = self.round_timeout_duration(next_round);
-        debug!(target: "rhd", "Advancing to round {} from {}", next_round, round);
-        debug!(target: "rhd", "Participating authorities: {:?}",
+	let round_timeout = self.round_timeout_duration(next_round);
+	debug!(target: "rhd", "Advancing to round {} from {}", next_round, round);
+	debug!(target: "rhd", "Participating authorities: {:?}",
 	       collect_pubkeys(accumulator.participants()));
-        debug!(target: "rhd", "Voting authorities: {:?}",
+	debug!(target: "rhd", "Voting authorities: {:?}",
 	       collect_pubkeys(accumulator.voters()));
-        debug!(target: "rhd", "Round {} should end in at most {} seconds from now", next_round, round_timeout.as_secs());
+	debug!(target: "rhd", "Round {} should end in at most {} seconds from now", next_round, round_timeout.as_secs());
 
-        self.update_round_cache(next_round);
+	self.update_round_cache(next_round);
 
-        if let AdvanceRoundReason::Timeout = reason {
-            self.proposer
-                .on_round_end(round, accumulator.proposal().is_some());
-        }
+	if let AdvanceRoundReason::Timeout = reason {
+	    self.proposer
+		.on_round_end(round, accumulator.proposal().is_some());
+	}
     }
 }
 
@@ -366,8 +359,8 @@ pub fn gen_rhd_worker_pair<B, E, I>(
     imported_block_rx: UnboundedReceiver<BlockImportParams>,
 ) -> Result<
     (
-        impl futures01::Future<Item = (), Error = ()>,
-        impl futures01::Future<Item = (), Error = ()>,
+	impl futures01::Future<Item = (), Error = ()>,
+	impl futures01::Future<Item = (), Error = ()>,
     ),
     sp_consensus::Error,
 >
@@ -380,21 +373,32 @@ where
 {
     // generate channels
     let (tc_tx, tc_rx, ts_tx, ts_rx) = gen::gen_consensus_msg_channels();
-    let (mb_tx, mb_rx) = gen::gen_mint_block_channel();
+    let (cb_tx, cb_rx) = gen::gen_commit_block_channel();
     let (ib_tx, ib_rx) = gen::gen_import_block_channel();
+    let (ap_tx, ap_rx) = gen::gen_ask_proposal_channel();
+    let (gp_tx, gp_rx) = gen::gen_give_proposal_channel();
 
     let bftml_worker = BftmlWorker::new(
-        client.clone(),
-        Arc::new(Mutex::new(block_import)),
-        proposer_factory,
-        imported_block_rx,
-        tc_tx,
-        ts_rx,
-        mb_rx,
-        ib_tx,
+	client.clone(),
+	Arc::new(Mutex::new(block_import)),
+	proposer_factory,
+	imported_block_rx,
+	tc_tx,
+	ts_rx,
+	cb_rx,
+	ib_tx,
+	ap_rx,
+	gp_tx,
     );
 
-    let rhd_worker = RhdWorker::new(tc_rx, ts_tx, mb_tx, ib_rx);
+    let rhd_worker = RhdWorker::new(
+	tc_rx,
+	ts_tx,
+	cb_tx,
+	ib_rx,
+	ap_tx,
+	gp_rx,
+    );
 
     Ok((bftml_worker, rhd_worker))
 }
