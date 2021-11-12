f9015a27e7c938bd8dc4f7e2524ff20ba934316c
Yuanchao Sun <yuanchao.sun@gmail.com>
2021-11-12 23:21:58 +0800

    Update parameters of pallet_octopus_appchain

--- bin/node-template/runtime/src/lib.rs
+++ bin/node-template/runtime/src/lib.rs
@@ -561,8 +561,8 @@ impl frame_system::offchain::AppCrypto<<Signature as Verify>::Signer, Signature>
 
 parameter_types! {
 	   pub const OctopusAppchainPalletId: PalletId = PalletId(*b"py/octps");
-	   pub const GracePeriod: u32 = 5;
-	   pub const UnsignedPriority: u64 = 1 << 20;
+	   pub const GracePeriod: u32 = 10;
+	   pub const UnsignedPriority: u64 = 1 << 21;
 	   pub const RequestEventLimit: u32 = 10;
 	   pub const UpwardMessagesLimit: u32 = 10;
 }
