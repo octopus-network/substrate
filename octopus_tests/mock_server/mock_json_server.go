f58e57d2f4efb799bffee91e168869fffcff819d
令狐一冲 <43949039+anonymousGiga@users.noreply.github.com>
2021-11-20 16:33:32 +0800

    Modify it-test (#52)
    * Modify mock server support the new request format
    
    * fix error
    
    * fix

--- octopus_tests/mock_server/mock_json_server.go
+++ octopus_tests/mock_server/mock_json_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
@@ -11,134 +12,12 @@ import (
 	"sync"
 	"time"
 
+	"encoding/base64"
 	"encoding/json"
+
+	producer "./mockdataproducer"
+	"github.com/tidwall/gjson"
 )
-
-//const (
-//	LockToken          = "LockToken"
-//	UpdateValidatorSet = "UpdateValidatorSet"
-//)
-
-type LockTokenData struct {
-	SeqNum   uint64 `json:"seq_num"`
-	TokenId  string `json:"token_id"`
-	SenderId string `json:"sender_id"`
-	Receiver string `json:"receiver"`
-	Amount   string `json:"amount"`
-}
-
-type ValidatorInfo struct {
-	Id        string
-	AccountId string
-	Weight    string
-}
-
-const PRESET_VALIDATORS_SIZE = 5
-
-//preset validators
-var presetValidators = [...]ValidatorInfo{
-	{"0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d", "Alice-octopus.testnet", "10000000000"},
-	{"0x8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48", "Bob-octopus.testnet", "10000000000"},
-	{"0x90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22", "Charlie-octopus.testnet", "100000000000"},
-	{"0x306721211d5404bd9da88e0204360a1a9ab8b87c66c1bc2fcdd37f3c2222cc20", "Dave-octopus.testnet", "10000000000"},
-	{"0xe659a7a1628cdd93febc04a4e0646ea20e9f5f0ce097d9a05290d4a9e054df4e", "Eve-octopus.testnet", "10000000000"},
-}
-
-type ValidatorData struct {
-	ValidatorId string `json:"validator_id_in_appchain"`
-	TotalStake  string `json:"total_stake"`
-}
-
-// type UpdateValidatorSetData struct {
-// 	SeqNum       uint64          `json:"seq_num"`
-// 	SetId        uint64          `json:"set_id"`
-// 	ValidatorSet []ValidatorData `json:"validators"`
-// }
-
-// type InnerResultValidatorSet struct {
-// 	UpdateValidatorSet UpdateValidatorSetData `json:"UpdateValidatorSet"`
-// }
-
-type InnerResultLockToken struct {
-	LockToken LockTokenData `json:"LockAsset"`
-}
-
-type ResultData struct {
-	BlockHash   string   `json:"block_hash"`
-	BlockHeight uint64   `json:"block_height"`
-	Logs        []string `json:"logs"`
-	InnerResult []int    `json:"result"` //it's UpdateValidatorSetData and LockTokenData
-}
-
-type Ret struct {
-	Jsonrpc string     `json:"jsonrpc"`
-	Id      string     `json:"id"`
-	Result  ResultData `json:"result"`
-}
-
-var blockHeight uint64 = 1
-
-func ProduceValidatorData(curr int) ValidatorData {
-	rand.Seed(time.Now().UnixNano())
-	num := rand.Intn(5)
-
-	if curr > PRESET_VALIDATORS_SIZE {
-		curr %= PRESET_VALIDATORS_SIZE
-	}
-	validator := presetValidators[curr]
-
-	if num%2 == 0 {
-		blockHeight++
-	}
-
-	return ValidatorData{
-		ValidatorId: validator.Id,
-		TotalStake:  validator.Weight,
-	}
-}
-
-var setId uint64 = 1
-var seqNum uint64 = 0
-
-func ProduceLockTokenData() LockTokenData {
-	ret := LockTokenData{
-		SeqNum:   seqNum,
-		TokenId:  "test-stable.testnet",
-		SenderId: "yuanchao.testnet",
-		Receiver: "0x90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22",
-		Amount:   "123000000000000",
-	}
-
-	seqNum++
-
-	return ret
-}
-
-var blockHeight2 uint64 = 1
-
-func StringToInts(s string) []int {
-	intSlice := make([]int, len(s))
-	for i, _ := range s {
-		intSlice[i] = int(s[i])
-	}
-	return intSlice
-}
-
-func ProduceUpdateValidatorSets(curr []int) []interface{} {
-	num := len(curr)
-
-	var validatorSet = []interface{}{}
-	for i := 0; i < num; i++ {
-		if curr[i] < 0 {
-			continue
-		}
-		data := ProduceValidatorData(curr[i])
-		validatorSet = append(validatorSet, data)
-	}
-
-	// fmt.Printf("%v\n", validatorSet...)
-	return validatorSet
-}
 
 // write mock data to file which used to compare
 func writeResult(vals [][]int, outfile string) error {
@@ -173,43 +52,13 @@ func writeResult(vals [][]int, outfile string) error {
 //  {2, ...},		// locktoken
 //  {3, ...}		// burn event
 //}
-func ProduceNewResponseForValidatorSets(simulationSequence [][]int) Ret {
-	var innerResult interface{}
-
-	if len(simulationSequence) != 1 {
-		panic("Should 1")
-	}
-
-	for i := 0; i < len(simulationSequence); i++ {
-		if simulationSequence[i][0] == 1 { //update validators
-			curr := simulationSequence[i][1:]
-			// innerResult = append(innerResult, ProduceUpdateValidatorSets(curr))
-			innerResult = ProduceUpdateValidatorSets(curr)
-		}
-	}
-
-	result, _ := json.Marshal(innerResult)
-
-	retData := ResultData{
-		BlockHash:   "EczErquQLMpUvTQpKupoQp5yNkgNbniMSHq1gVvhAf84", //mock hash
-		BlockHeight: blockHeight2,
-		Logs:        []string{},
-		InnerResult: StringToInts(string(result)),
-	}
-
-	return Ret{
-		Jsonrpc: "2.0",
-		Id:      "dontcare",
-		Result:  retData,
-	}
-}
 
 //case 1
 func Test1() [][]int {
 	mockData := [][]int{
 		{1, 0, 1, 2, 3},
-		{1, 3, 4, 1},
-		{1, 0, 2},
+		{1, 0, 1, 2},
+		{1, 0, 1},
 		{1, 3, 2, 1},
 		{1, 4, 1, 2, 3},
 		{1, 0, 1, 2, 3, 4},
@@ -222,12 +71,12 @@ func Test1() [][]int {
 
 var testData [][]int
 var once sync.Once
-var currRet Ret
+var currRet producer.Ret
 var preTime int64 = 0
 var startLine int = 0
 var endLine int = 0
 
-func ProduceResponse() Ret {
+func ProduceValidatorResponse(eraNumber uint32) producer.Ret {
 	//test use case
 	once.Do(func() {
 		testData = Test1()
@@ -236,7 +85,7 @@ func ProduceResponse() Ret {
 	//produce responce data
 	currTime := time.Now().Unix()
 	deltTime := currTime - preTime
-	if (preTime == 0) || (deltTime > 60*2 && endLine < len(testData)) {
+	if (preTime == 0) || (deltTime > 60*8 && endLine < len(testData)) {
 		rand.Seed(time.Now().UnixNano())
 		// delt := rand.Intn(len(testData))
 		delt := 1
@@ -245,7 +94,7 @@ func ProduceResponse() Ret {
 			endLine = len(testData)
 		}
 
-		currRet = ProduceNewResponseForValidatorSets(testData[startLine:endLine])
+		currRet = producer.ProduceNewResponseForValidatorSets(testData[startLine:endLine])
 		fmt.Printf("start: %v, end: %v\n", startLine, endLine)
 		startLine = endLine
 		preTime = currTime
@@ -254,12 +103,26 @@ func ProduceResponse() Ret {
 	return currRet
 }
 
+func ProduceNotifyResponse(startIndex uint32, quantity uint32) producer.Ret {
+	//test use case
+	testData := []producer.SimulationData{}
+	s1 := producer.SimulationData{
+		Receiver: producer.PresetValidators[0].Id,
+		Amount:   "10000",
+		DataType: producer.BurntToken,
+	}
+	testData = append(testData, s1)
+
+	ret := producer.ProduceNewResponseForNotifyHistories(testData)
+	return ret
+}
+
 type ParamsData struct {
-	RequestType string
-	Finality    string
-	AccountId   string
-	MethodName  string
-	ArgsBase64  string
+	RequestType string `json:"request_type"`
+	Finality    string `json:"finalty"`
+	AccountId   string `json:"account_id"`
+	MethodName  string `json:"method_name"`
+	ArgsBase64  string `json:"args_base64"`
 }
 
 type Req struct {
@@ -273,6 +136,48 @@ var handleCnt uint64 = 0
 
 const HANDLER_TIMES = 200000
 
+func DecodeFromBase64(data string) (string, error) {
+	sDec, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		fmt.Printf("Error decoding string: %s ", err.Error())
+		return "", err
+	}
+
+	return string(sDec), nil
+}
+
+func ParseBody(body []byte) map[string]interface{} {
+	var req Req
+	req.Jsonrpc = gjson.Get(string(body), "jsonrpc").String()
+	req.Id = gjson.Get(string(body), "id").String()
+	req.Method = gjson.Get(string(body), "method").String()
+	req.Params.RequestType = gjson.Get(string(body), "params.request_type").String()
+	req.Params.AccountId = gjson.Get(string(body), "params.account_id").String()
+	req.Params.MethodName = gjson.Get(string(body), "params.method_name").String()
+	req.Params.ArgsBase64 = gjson.Get(string(body), "params.args_base64").String()
+
+	// fmt.Printf("%+v\n", req)
+
+	s, err := DecodeFromBase64(req.Params.ArgsBase64)
+	if err != nil {
+		fmt.Println("Parse error")
+	}
+
+	res := make(map[string]interface{})
+	res["method_name"] = req.Params.MethodName
+	switch req.Params.MethodName {
+	case "get_appchain_notification_histories":
+		res["start_index"] = gjson.Get(s, "start_index").Uint()
+		res["quantity"] = gjson.Get(s, "quantity").Uint()
+	case "get_validator_list_of":
+		res["era_number"] = gjson.Get(s, "era_number").Uint()
+	default:
+		fmt.Println("Not found match method!")
+	}
+
+	return res
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		if handleCnt > HANDLER_TIMES {
@@ -281,28 +186,28 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		handleCnt++
 	}()
 
-	//fmt.Println("method:", r.Method)
-	//body, err := ioutil.ReadAll(r.Body)
-	//if err != nil {
-	//	fmt.Printf("read body err, %v\n", err)
-	//	return
-	//}
-	//println("request json:", string(body))
+	// fmt.Println("method:", r.Method)
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("read body err, %v\n", err)
+		return
+	}
+	// println("request json:", string(body))
 
-	////parse json to struct
-	//var req Req
-	//req.Jsonrpc = gjson.Get(string(body), "jsonrpc").String()
-	//req.Id = gjson.Get(string(body), "id").String()
-	//req.Method = gjson.Get(string(body), "method").String()
-	//req.Params.RequestType = gjson.Get(string(body), "params.request_type").String()
-	//req.Params.AccountId = gjson.Get(string(body), "params.account_id").String()
-	//req.Params.MethodName = gjson.Get(string(body), "params.method_name").String()
-	//req.Params.ArgsBase64 = gjson.Get(string(body), "params.args_base64").String()
-
-	//fmt.Printf("%+v\n", req)
+	//parse request
+	pb := ParseBody(body)
+	// fmt.Println("pb === ", pb)
 
 	//dealt the req
-	response := ProduceResponse()
+	var response producer.Ret
+	switch pb["method_name"] {
+	case "get_validator_list_of":
+		response = ProduceValidatorResponse(uint32(pb["era_number"].(uint64)))
+	case "get_appchain_notification_histories":
+		response = ProduceNotifyResponse(uint32(pb["start_index"].(uint64)), uint32(pb["quantity"].(uint64)))
+	default:
+		fmt.Println("Not found match method!")
+	}
 
 	w.Header().Set("content-type", "text/json")
 	ret, err := json.Marshal(response)
--- octopus_tests/modify.sh
+++ octopus_tests/modify.sh
@@ -12,6 +12,13 @@ sed -i 's/authority_keys_from_seed("Alice"), authority_keys_from_seed("Bob")/\
 					authority_keys_from_seed("Eve"),\
 				/g' ../bin/node-template/node/src/chain_spec.rs
 
-sed -i "s/https:\/\/rpc.testnet.near.org/http:\/\/127.0.0.1:8080\/handler/g" ../bin/node-template/octopus-pallets/appchain/src/mainchain.rs
-line=`sed -n "/url/p" ../bin/node-template/octopus-pallets/appchain/src/mainchain.rs`
+sed -i "s/https:\/\/rpc.testnet.near.org/http:\/\/127.0.0.1:8080\/handler/g" ../bin/node-template/octopus-pallets/appchain/src/lib.rs
+#line=`sed -n "/url/p" ../bin/node-template/octopus-pallets/appchain/src/lib.rs`
+#echo "$line"
+
+sed -i "s/<PalletAccount<T>>::put(account_id);/<PalletAccount<T>>::put(account_id);\
+	<IsActivated<T>>::put(true);/g" ../bin/node-template/octopus-pallets/appchain/src/lib.rs
+
+sed -i "s/pub const EPOCH_DURATION_IN_BLOCKS\: BlockNumber = 10 \* MINUTES/pub const EPOCH_DURATION_IN_BLOCKS\: BlockNumber = 1 \* MINUTES/g" ../bin/node-template/runtime/src/lib.rs
+line=`sed -n "/pub const EPOCH_DURATION_IN_BLOCKS\: BlockNumber =/p" ../bin/node-template/runtime/src/lib.rs `
 echo "$line"
