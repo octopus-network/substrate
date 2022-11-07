18e52b6c6ded9551154b836101f037f59fef3dd6
Davirain <davirain.yin@gmail.com>
2022-11-07 14:31:40 +0800

    remove ics2-transfer

--- frame/ibc/src/context.rs
+++ frame/ibc/src/context.rs
@@ -1,8 +1,7 @@
 use crate::Config;
 use sp_std::marker::PhantomData;
 
-use ibc::{applications::transfer::MODULE_ID_STR, core::ics26_routing::context::RouterBuilder};
-use pallet_ics20_transfer::ics20_callback::IbcTransferModule;
+use ibc::core::ics26_routing::context::RouterBuilder;
 
 /// A struct capturing all the functional dependencies (i.e., context)
 /// which the ICS26 module requires to be able to dispatch and process IBC messages.
@@ -17,8 +16,6 @@ pub struct Context<T: Config> {
 impl<T: Config> Context<T> {
 	pub fn new() -> Self {
 		let r = SubstrateRouterBuilder::default()
-			.add_route(MODULE_ID_STR.parse().unwrap(), IbcTransferModule(PhantomData::<T>)) // register transfer Module
-			.unwrap()
 			.build();
 
 		Self { _pd: PhantomData::default(), router: r }
